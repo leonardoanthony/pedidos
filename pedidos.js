c307bd94f5fa9b6dd1c59433e4b0578447bb3d95
leonardoanthony <leonardoanthony.dev@gmail.com>
2025-04-30 17:39:44 -0300

    :sparkles: feat: painel adm

---
+++ pedidos.js
@@ -0,0 +1,69 @@
+const pedidos = [
+    {
+      code: 3425,
+      nome: "Denis",
+      status: "Entregue",
+      itens: [
+        { item: "Azul BB", tamanho: "M", quantidade: 2, valorUnitario: 40 },
+        { item: "Preto", tamanho: "M", quantidade: 2, valorUnitario: 40 },
+        { item: "Off White", tamanho: "M", quantidade: 1, valorUnitario: 40 },
+        { item: "Goiaba", tamanho: "M", quantidade: 2, valorUnitario: 40 }
+      ],
+      pagamentos: [
+        { mes: "Maio", valor: 80, status: "Em Aberto" },
+        { mes: "Junho", valor: 100, status: "Em Aberto" },
+        { mes: "Julho", valor: 100, status: "Em Aberto" }
+      ]
+    },
+    {
+      code: 4658,
+      nome: "Riva",
+      status: "Entregue",
+      itens: [
+        { item: "Cinza Escuro", tamanho: "G", quantidade: 1, valorUnitario: 40 },
+      ],
+      pagamentos: [
+        { mes: "Maio", valor: 40, status: "Em Aberto" },
+      ]
+    },
+    {
+      code: 5373,
+      nome: "Gabriel",
+      status: "Entregue",
+      itens: [
+        { item: "Verde acqua", tamanho: "G", quantidade: 1, valorUnitario: 40 },
+        { item: "Azul BB", tamanho: "G", quantidade: 1, valorUnitario: 40 },
+        { item: "Goiaba", tamanho: "P", quantidade: 1, valorUnitario: 40 },
+      ],
+      pagamentos: [
+        { mes: "Maio", valor: 60, status: "Em Aberto" },
+        { mes: "Junho", valor: 60, status: "Em Aberto" },
+      ]
+    },
+    {
+      code: 4812,
+      nome: "João",
+      status: "Entregue",
+      itens: [
+        { item: "Preta", tamanho: "G", quantidade: 1, valorUnitario: 40 },
+        { item: "Azul Marinho", tamanho: "G", quantidade: 1, valorUnitario: 40 },
+      ],
+      pagamentos: [
+        { mes: "Maio", valor: 40, status: "Pago" },
+        { mes: "Junho", valor: 40, status: "Pago" },
+      ]
+    },
+    {
+      code: 1152,
+      nome: "Andréia",
+      status: "Entregue",
+      itens: [
+        { item: "Verde Acqua", tamanho: "P", quantidade: 1, valorUnitario: 40 },
+        { item: "Preta", tamanho: "P", quantidade: 1, valorUnitario: 40 },
+      ],
+      pagamentos: [
+        { mes: "Maio", valor: 40, status: "Em Aberto" },
+        { mes: "Junho", valor: 40, status: "Em Aberto" },
+      ]
+    },
+  ];
