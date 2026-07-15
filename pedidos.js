5a0c3e4b19cfe43072351b1240fc70e6cdf66fed
Leonardo Anthony <67436042+leonardoanthony@users.noreply.github.com>
2026-07-15 10:44:43 -0300

    Add new order with details for Sylvia

--- pedidos.js
+++ pedidos.js
@@ -16,4 +16,21 @@ const pedidos = [
         { mes: "Novembro", valor: 85, status: "Em Aberto" }
       ]
     },
+    {
+      code: 0002,
+      nome: "Sylvia",
+      status: "Entregue",
+      itens: [
+        { item: "Pack Camisa 3 Unidades", tamanho: "GG", quantidade: 1, valorUnitario: 120 },
+        { item: "Sapato Nike - Preto", tamanho: "42", quantidade: 1, valorUnitario: 10 },
+        { item: "Sapato New Balance - Bege", tamanho: "42", quantidade: 1, valorUnitario: 10 },
+        { item: "Frete", tamanho: "", quantidade: 1, valorUnitario: 10 }
+      ],
+      pagamentos: [
+        { mes: "Agosto", valor: 10, status: "Em Aberto" },
+        { mes: "Setembro", valor: 10, status: "Em Aberto" },
+        { mes: "Outubro", valor: 10, status: "Em Aberto" },
+        { mes: "Novembro", valor: 10, status: "Em Aberto" }
+      ]
+    },
   ];
