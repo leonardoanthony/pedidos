41ad0b83868e99176043fba4c8125ec06f28441d
Leonardo Anthony <67436042+leonardoanthony@users.noreply.github.com>
2026-07-15 10:34:30 -0300

    Refactor pedidos data structure
    Removed multiple pedidos and updated existing ones.

--- pedidos.js
+++ pedidos.js
@@ -1,71 +1,19 @@
 const pedidos = [
     {
-      code: 3425,
-      nome: "Denis",
+      code: 1212,
+      nome: "Edgard",
       status: "Entregue",
       itens: [
-        { item: "Azul BB", tamanho: "M", quantidade: 2, valorUnitario: 40 },
-        { item: "Preto", tamanho: "M", quantidade: 2, valorUnitario: 40 },
-        { item: "Off White", tamanho: "M", quantidade: 2, valorUnitario: 40 },
-        { item: "Goiaba", tamanho: "M", quantidade: 2, valorUnitario: 40 }
+        { item: "Pack Camisa 3 Unidades", tamanho: "GG", quantidade: 1, valorUnitario: 90 },
+        { item: "Sapato Nike - Preto", tamanho: "42", quantidade: 1, valorUnitario: 120 },
+        { item: "Sapato New Balance - Bege", tamanho: "42", quantidade: 1, valorUnitario: 120 },
+        { item: "Frete", tamanho: "", quantidade: 1, valorUnitario: 10 }
       ],
       pagamentos: [
-        { mes: "Maio", valor: 80, status: "Em Aberto" },
-        { mes: "Junho", valor: 120, status: "Em Aberto" },
-        { mes: "Julho", valor: 120, status: "Em Aberto" }
-      ]
-    },
-    {
-      code: 4658,
-      nome: "Riva",
-      status: "Entregue",
-      itens: [
-        { item: "Cinza Escuro", tamanho: "G", quantidade: 1, valorUnitario: 40 },
-      ],
-      pagamentos: [
-        { mes: "Maio", valor: 40, status: "Pago" },
-      ]
-    },
-    {
-      code: 5373,
-      nome: "Gabriel",
-      status: "Entregue",
-      itens: [
-        { item: "Verde acqua", tamanho: "G", quantidade: 1, valorUnitario: 40 },
-        { item: "Verde acqua", tamanho: "M", quantidade: 1, valorUnitario: 40 },
-        { item: "Azul BB", tamanho: "G", quantidade: 1, valorUnitario: 40 },
-        { item: "Goiaba", tamanho: "P", quantidade: 1, valorUnitario: 40 },
-      ],
-      pagamentos: [
-        { mes: "Maio", valor: 60, status: "Pago" },
-        { mes: "Junho", valor: 60, status: "Em Aberto" },
-        { mes: "Julho", valor: 40, status: "Em Aberto" },
-      ]
-    },
-    {
-      code: 4812,
-      nome: "João",
-      status: "Entregue",
-      itens: [
-        { item: "Preta", tamanho: "G", quantidade: 1, valorUnitario: 40 },
-        { item: "Azul Marinho", tamanho: "G", quantidade: 1, valorUnitario: 40 },
-      ],
-      pagamentos: [
-        { mes: "Maio", valor: 40, status: "Pago" },
-        { mes: "Junho", valor: 40, status: "Pago" },
-      ]
-    },
-    {
-      code: 1152,
-      nome: "Andréia",
-      status: "Entregue",
-      itens: [
-        { item: "Verde Acqua", tamanho: "P", quantidade: 1, valorUnitario: 40 },
-        { item: "Preta", tamanho: "P", quantidade: 1, valorUnitario: 40 },
-      ],
-      pagamentos: [
-        { mes: "Maio", valor: 40, status: "Pago" },
-        { mes: "Junho", valor: 40, status: "Em Aberto" },
+        { mes: "Agosto", valor: 85, status: "Em Aberto" },
+        { mes: "Setembro", valor: 85, status: "Em Aberto" },
+        { mes: "Outubro", valor: 85, status: "Em Aberto" },
+        { mes: "Novembro", valor: 85, status: "Em Aberto" }
       ]
     },
   ];
